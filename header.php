d5db324c06954e52d51111b87fcd3f8387df238d
Shubham Jain <shbhm89300@gmail.com>
2021-07-03 16:16:49 +0530

    Adding header and footer

--- header.php
+++ header.php
@@ -5,4 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website</title>
+
+    <?php 
+        wp_head();
+    ?>
+</head>
+
+<body>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li>Logo</li>
+            </ul>
+            <ul>
+                <li>About</li>
+                <li>Blogs</li>
+                <li>Posts</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
