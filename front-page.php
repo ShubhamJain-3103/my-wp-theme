f8ac36ae7fef212020cd13d0e6a6f086adc2b014
Shubham Jain <shbhm89300@gmail.com>
2021-07-03 16:17:57 +0530

    Adding front page static content

--- front-page.php
+++ front-page.php
@@ -1,17 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+    get_header();
+?>
+<!-- <div class="container">
+    <div class="col-1">
+        <img src="" alt="" />
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio laborum recusandae animi inventore
+            culpa delectus sunt ad omnis rem corrupti eos eaque provident ducimus consectetur, officiis
+            exercitationem nam necessitatibus tempore!</p>
+    </div>
+    <div class="col-2">
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+        <div class="posts">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit numquam quaerat facere quas,
+                atque sed assumenda ipsum vitae consequatur harum necessitatibus accusamus eveniet quia magnam culpa
+                modi pariatur possimus.</p>
+        </div>
+    </div>
+</div> -->
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php 
-        wp_head();
-    ?>
-</head>
+<article>
+    <h1><?php the_title();?></h1>
+    <h3>
+        <?php
+        if(have_posts()){
+            while(have_posts()){
+                the_post();
+                the_content();
+            }
+        }
+        ?>
+    </h3>
+</article>
 
-<body>
-</body>
-
-</html>
+<?php
+    get_footer();
+?>
