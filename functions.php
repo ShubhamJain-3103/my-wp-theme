71cd50197f26a98188a75c14b78fddf34de72b0a
Shubham Jain <shbhm89300@gmail.com>
2021-07-03 16:16:19 +0530

    Adding css and js

--- functions.php
+++ functions.php
@@ -1,11 +1,40 @@
 <?php
 
-echo "Hello Functions";
+function mytheme_title_support(){
+
+    add_theme_support('title-tag');
+
+}
+
+add_action( "after_setup_theme","mytheme_title_support");
 
 function mytheme_register_styles(){
-    wp_enqueue_style( 'mytheme-main-style', get_template_directory_uri(  )."/style.css", array(), "1.0","all" );
+    $version = wp_get_theme()->get('Version');
+    
+    wp_enqueue_style(
+        'mytheme-main-style',
+        get_template_directory_uri()."/style.css", 
+        array(), 
+        $version,
+        "all"
+    );
+    // array is used to order the styles
 }
 
 add_action("wp_enqueue_scripts","mytheme_register_styles",1);
+
+function mytheme_register_scripts(){
+    wp_enqueue_script(
+        'mytheme-main-javascript',
+        get_template_directory_uri()."/assests/main.js",
+        array(),
+        '1.0',
+        true
+    );
+}
+add_action( "wp_enqueue_scripts", "mytheme_register_scripts");
+
+
+
 
 ?>
